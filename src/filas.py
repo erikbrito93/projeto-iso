9e367982d516852c7c3a05a365ec281e5e115d6f
Rod <rodrigo.teixeira0@gmail.com>
2022-04-15 20:13:07 -0300

    Implementa preempção em processos de usuário e mudança de prioridade.

--- src/filas.py
+++ src/filas.py
@@ -13,32 +13,42 @@ class QueueManager:
 		self.current_process = {}
 		self.last_PID = 0
 		self.is_finished = False
+		self.quantum = 5
 
-	def createProcess(self, start_time, priority, processing_time, memory_blocks, printer_code, scanner_req, modem_req, disk_code):
+	def createProcess(self, start_time, priority, processing_time, memory_blocks, printer_code, scanner_req, modem_req, driver_code):
 		self.last_PID += 1
-		newProcess = Process(self.last_PID, start_time, priority, processing_time, memory_blocks, printer_code, scanner_req, modem_req, disk_code)
-		print("\nProcess created:")
-		print("PID: " + str(newProcess.PID))
-		print("Start time: " + str(newProcess.start_time))
-		print("Priority: " + str(newProcess.priority))
-		print("Processing Time: " + str(newProcess.processing_time))
-		print("Memory Blocks: " + str(newProcess.memory_blocks))
-		print("Printers: " + str(newProcess.printer_code))
-		print("Scanners: " + str(newProcess.scanner_req))
-		print("Modems: " + str(newProcess.modem_req))
-		print("Disks: " + str(newProcess.disk_code))
+		newProcess = Process(self.last_PID, start_time, priority, processing_time, memory_blocks, printer_code, scanner_req, modem_req, driver_code)
 		self.processes.append(newProcess)
 
 	def orderProcesses(self):
 		self.processes.sort(key=lambda process: process.start_time)
+
+	def announceProcess(self, process):
+		print("\nProcess created:")
+		print("PID: " + str(process.PID))
+		print("Priority: " + str(process.priority))
+		print("Processing Time: " + str(process.processing_time))
+		print("Offset: 0")
+		print("Memory Blocks: " + str(process.memory_blocks))
+		print("Printers: " + str(process.printer_code))
+		print("Scanners: " + str(process.scanner_req))
+		print("Modems: " + str(process.modem_req))
+		print("Drivers: " + str(process.driver_code))
+		print ("\n")
 
 	def enqueueProcesses(self, time):
 		if (len(self.processes) > 0):
 			process_to_enqueue = self.processes[0]
 			if (process_to_enqueue.priority == 0 and time >= process_to_enqueue.start_time):
 				self.real_time_processes.put(self.processes.pop(0))
+				self.announceProcess(process_to_enqueue)
 			elif (time >= process_to_enqueue.start_time):
 				self.user_processes.put(self.processes.pop(0))
+				self.announceProcess(process_to_enqueue)
+		
+		if (len(self.processes) > 0):
+			if (self.processes[0].start_time == time):
+				self.enqueueProcesses(time)
 
 		while (not self.user_processes.empty()):
 			process_to_enqueue = self.user_processes.get()
@@ -54,6 +64,13 @@ class QueueManager:
 		time = 0
 		while (not self.is_finished):
 			self.enqueueProcesses(time)
+			
+			if (not self.real_time_processes.empty() and self.current_process != {}):
+				if (self.current_process.priority > 0):
+					self.user_processes.put(self.current_process)
+					self.current_process = {}
+					print ("\n")
+
 			if (self.current_process == {}):
 				if (not self.real_time_processes.empty()):
 					self.current_process = self.real_time_processes.get()
@@ -63,11 +80,22 @@ class QueueManager:
 					self.current_process = self.user_processes2.get()
 				elif (not self.user_processes3.empty()):	
 					self.current_process = self.user_processes3.get()
+
 			if (self.current_process != {}):			
 				if (time >= self.current_process.start_time):
 					self.current_process.runInstruction()
+					self.current_process.current_execution_time += 1
+
 				if (self.current_process.is_finished):
 					self.current_process = {}
+					print ("\n")
+				elif (self.current_process.current_execution_time >= self.quantum and self.current_process.priority > 0):
+					if (self.current_process.priority < 3):
+						self.current_process.priority += 1
+					self.current_process.current_execution_time = 0
+					self.user_processes.put(self.current_process)
+					self.current_process = {}
+					print ("\n")
 
 			if (len(self.processes) == 0 and self.real_time_processes.empty() and self.user_processes1.empty() and self.user_processes2.empty() and self.user_processes3.empty() and self.current_process == {}):
 				self.is_finished = True
--- src/processos.py
+++ src/processos.py
@@ -1,5 +1,5 @@
 class Process:
-	def __init__(self, PID, start_time, priority, processing_time, memory_blocks, printer_code, scanner_req, modem_req, disk_code):
+	def __init__(self, PID, start_time, priority, processing_time, memory_blocks, printer_code, scanner_req, modem_req, driver_code):
 		self.PID = PID
 		self.start_time = start_time
 		self.priority = priority
@@ -8,17 +8,19 @@ class Process:
 		self.printer_code = printer_code
 		self.scanner_req = scanner_req
 		self.modem_req = modem_req
-		self.disk_code = disk_code
+		self.driver_code = driver_code
 		self.execution_time = 0
 		self.is_finished = False
+		self.current_execution_time = 0
 
 	def runInstruction(self):
 		if (self.execution_time == 0):
-			print ("\nP" + str(self.PID) + " STARTED")
+			print ("P" + str(self.PID) + " STARTED")
+		if (self.execution_time < self.processing_time):
+			self.execution_time += 1 
+			print ("P" + str(self.PID) + " instruction " + str(self.execution_time))			
 		if (self.execution_time >= self.processing_time):
 			self.is_finished = True
 			print ("P" + str(self.PID) + " return SIGINT")
-		else:
-			self.execution_time += 1 
-			print ("P" + str(self.PID) + " instruction " + str(self.execution_time))
+			
 
